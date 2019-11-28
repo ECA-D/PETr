cc928bda86eb505baf4629cf7977f51a4bad3fe1
colin-harpham <c.harpham@uea.ac.uk>
2019-11-28 15:35:20 +0000

    minor update

--- R/pet_penmon.r
+++ R/pet_penmon.r
@@ -17,9 +17,9 @@
 #' dp =  Dewpoint           (degrees Celsius)
 #' ws  = Wind speed at 10m  (m/s)
 #' either ss or rs is passed, but not both
-#' sun  = Sunshine duration  (0-24 hours)
+#' ss  = Sunshine duration  (0-24 hours)
 #' cl  = Cloudiness         (0-1)
-#' cl is optional for use with sun, if unavailble cl is estimated
+#' cl is optional for use with ss, if unavailble cl is estimated
 #' rs  = radiation          (kwh/m^2)
 #' Also rquired is the latitude and longitude
 #' lat = Latitude  (degrees)
@@ -31,16 +31,23 @@
 #' @export
 
 pet_penmon <- function(indat) {
-#browser()
   data("elev_dat", package="PETr")
-
+#browser()
   lat <- indat$lat
   lon <- indat$lons
   # lat index
   lat_index <- which.min(abs(lat - elev_lat))
   # lon index
   lon_index <- which.min(abs(lon - elev_lon))
+  tm <- rowMeans(cbind(indat$tmin, indat$tmax))
+  ndat <- length(tm)
+
   elev <- elev_dat[lon_index, lat_index]
+  if(is.na(elev))
+  {
+    pet <- array(dim=ndat, NA)
+    return(pet)
+  }
 
   jd <- as.POSIXlt(indat$tmax.dates)$yday + 1
   # real ssf      # Sunshine factor
@@ -55,26 +62,25 @@ pet_penmon <- function(indat) {
   # real dyln     # Day length
   # real psy      # Psychrometric constant set to the same as in SWURVE 0.067 kPaC-1
 
-  tm <- rowMeans(cbind(indat$tmin, indat$tmax))
-  ndat <- length(tm)
 
   # check whether vp, rh or dp are present
-  if (is.null(indat$vp) &&  !is.null(indat$rh)) {
+  if (is.null(indat$vp) && !is.null(indat$rh)) {
     # estimate vp from rh
     vps <- vapour_pressure(tm)
     vp <- (indat$rh / 100) * vps
 
-  } else if (is.null(vp) &&  !is.null(indat$dp)) {
+  } else if (is.null(indat$vp) &&  !is.null(indat$dp)) {
     # estimate vp from dp
-    rh <- dewpoint_to_humidity(dp, tm)
+    vps <- vapour_pressure(tm)
+    rh <- dewpoint_to_humidity(indat$dp, tm)
     vp <- (rh / 100) * vps
 
   }  else if (!is.null(vp)) {
     vp <- indat$vp
   }
 
-  # check for pressence of sun and cl
-  if (is.null(indat$sun)) {
+  # check for pressence of ss and cl
+  if (is.null(indat$ss)) {
     ss_pres <- FALSE
   } else {
     ss_pres <- TRUE
@@ -109,8 +115,8 @@ pet_penmon <- function(indat) {
   dr <- 1.0 + 0.033 * cos(0.0172 * jd)
   # solar constant
   Gsc <- 0.0820
-  ktn <- indat$tmin + 273.16
-  ktx <- indat$tmax + 273.16
+  ktmin <- indat$tmin + 273.16
+  ktmax <- indat$tmax + 273.16
   # deals with S. Hemisphere...
   cos_snst <- (-1) * tan(latr) * tan(slrd)
   cos_snst[cos_snst > 1] <- 1
@@ -131,27 +137,27 @@ pet_penmon <- function(indat) {
       dyln_zero <- which(dyln < 0.000001)
       dyln_nonzero <- which(dyln >= 0.000001)
       cl[dyln_zero] <- 1.0
-      cl[dyln_nonzero] <- 1.0 - indat$sun[dyln_nonzero] / dyln[dyln_nonzero]
+      cl[dyln_nonzero] <- 1.0 - indat$ss[dyln_nonzero] / dyln[dyln_nonzero]
     }
     ssf <- 1.0 - cl
 
     rns <- 0.77 * (0.25 + (0.50 * (ssf))) * dter
-    rnl <- sigma * (0.9 * (ssf) + 0.1) * (0.34-0.14*sqrt(avp)) * (ktn^4.0 + ktx^4.0)
+    rnl <- sigma * (0.9 * (ssf) + 0.1) * (0.34-0.14*sqrt(avp)) * (ktmin^4.0 + ktmax^4.0)
   } else {
 
     rso <- (0.75 + (2 * 10^-5) * elev) * dter
     rns <- (1 - a) * indat$rs
     rs_div_rso <- indat$rs / rso
     rs_div_rso[rs_div_rso > 1.0] <- 1.0
-    rnl <- sigma * (0.34 - 0.14 * sqrt(avp)) * (ktx^4 + ktn^4)/2 * (1.35 * rs_div_rso - 0.35)
+    rnl <- sigma * (0.34 - 0.14 * sqrt(avp)) * (ktmax^4 + ktmin^4)/2 * (1.35 * rs_div_rso - 0.35)
     rnl[rnl < 0] <- 0
   }
   # Net radiation
   rnet <- rns - rnl
-  vptn <- 0.1 * vapour_pressure(indat$tmin)
-  vptx <- 0.1 * vapour_pressure(indat$tmax)
-  vpd <- ((vptx + vptn) / 2.0) - avp
-
+  vptmin <- 0.1 * vapour_pressure(indat$tmin)
+  vptmax <- 0.1 * vapour_pressure(indat$tmax)
+  vpd <- ((vptmax + vptmin) / 2.0) - avp
+#browser()
   pet <- 0.408 * dvp * (rnet - tflux)
   pet <- pet + (900.0 / (tm + 273.16)) * (psy * wn2m * vpd)
   pet <- pet / (dvp + (psy * (1.0 + (wn2m * 0.34))))
@@ -185,9 +191,8 @@ dewpoint_to_humidity <- function (dp = NA, t = NA)
     stop("The vectors for temperature('t') and dewpoint temperature ('dp') must have the same length.")
   }
   if (length(dp[dp > t & !is.na(dp) & !is.na(t)]) > 0) {
-    dp[dp > t] <- NA
-    warning("For some observations, dew point temperature was higher than temperature.
-            Since dew point temperature cannot be higher than air temperature, relative humidty for these observations was set to 'NA'.")
+ #   browser()
+    dp[dp > t] <- t[dp > t]
   }
   beta <- (112 - (0.1 * t) + dp) / (112 + (0.9 * t))
   relative.humidity <- beta^8
